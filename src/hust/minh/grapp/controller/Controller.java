beca554c93b517ff94a64eb83d748647bc079244
Minh <tuanminhnguyen3395@gmail.com>
2017-11-18 14:10:58 +0700

    Clean up and add Patient model

---
+++ src/hust/minh/grapp/controller/Controller.java
@@ -0,0 +1,4 @@
+package hust.minh.grapp.controller;
+
+public class Controller {
+}
--- src/hust/minh/grapp/model/Patient.java
+++ src/hust/minh/grapp/model/Patient.java
@@ -1,22 +1,87 @@
 package hust.minh.grapp.model;
 
-import java.util.Date;
+import javafx.beans.property.*;
+
+import java.time.LocalDate;
 
 public class Patient {
-    public enum Gender {MALE, FEMALE, OTHER}
+    private final IntegerProperty _id;
+    private final StringProperty _firstName;
+    private final StringProperty _lastName;
+    private final StringProperty _gender;
+    private final ObjectProperty<LocalDate> _dob;
+    private final StringProperty _address;
+    private final StringProperty _phoneNumber;
+    private final StringProperty _email;
 
-    private int _id;
-    private String _firstName;
-    private String _lastName;
-    private Gender _gender;
-    private Date _dob;
-    private String _address;
-    private String _phoneNumber;
-    private String _email;
-
-    public  Patient(int id)
-    {
-        _id = id;
+    public Patient(int id) {
+        _id = new SimpleIntegerProperty(id);
+        _firstName = new SimpleStringProperty("Minh");
+        _lastName = new SimpleStringProperty("Nguyen");
+        _gender = new SimpleStringProperty("Male");
+        _dob = new SimpleObjectProperty(LocalDate.of(1995, 3, 3));
+        _address = new SimpleStringProperty("Hanoi");
+        _phoneNumber = new SimpleStringProperty("1234567890");
+        _email = new SimpleStringProperty("minh@gmail.com");
     }
 
+    public void setFirstName(String firstName) {
+        _firstName.set(firstName);
+    }
+
+    public void setLastName(String lastName) {
+        _lastName.set(lastName);
+    }
+
+    public void setGender(String gender) {
+        _gender.set(gender);
+    }
+
+    public void setBirthdate(int year, int month, int day) {
+        _dob.set(LocalDate.of(year, month, day));
+    }
+
+    public void setAddress(String address) {
+        _address.set(address);
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        _phoneNumber.set(phoneNumber);
+    }
+
+    public void setEmail(String email) {
+        _email.set(email);
+    }
+
+    public int getId() {
+        return _id.get();
+    }
+
+    public String getFirsName() {
+        return _firstName.get();
+    }
+
+    public String getLasName() {
+        return _lastName.get();
+    }
+
+    public String getGender() {
+        return _gender.get();
+    }
+
+    public LocalDate getBirthdate() {
+        return _dob.get();
+    }
+
+    public String getAddress() {
+        return _address.get();
+    }
+
+    public String getPhoneNumber() {
+        return _phoneNumber.get();
+    }
+
+    public String getEmail() {
+        return _email;
+    }
 }
